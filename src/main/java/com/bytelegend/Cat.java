a7ab67d0fb4f050dd04ce57783828d3dfb884204
Yeely0162 <Yeely01621@163.com>
2022-05-11 08:36:13 +0000

    Update at 2022-05-11T08:36:13.659589Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,17 +3,21 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    public Cat(String name) {
+        this.name=name;
+    }
+
     public static void main(String[] args) {
         Cat tom = createNewCat("Tom");
         tom.sayName();
     }
 
     public static Cat createNewCat(String name) {
-        return new Cat();
+        return new Cat(name);
     }
 
     // Print my `name` to standard output
     public void sayName() {
-        System.out.println("My name is " + this.name + ", my name's length is " + this.name.length());
+        System.out.println("My name is " + name + ", my name's length is " + name.length());
     }
 }
