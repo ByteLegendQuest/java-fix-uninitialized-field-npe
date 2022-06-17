60c612fa22d4c2d4ae05f41e7a661e23b21c11a1
karolk75 <karolk410410@gmail.com>
2022-06-17 19:30:09 +0000

    Update at 2022-06-17T19:30:09.243775Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,20 +1,24 @@
 package com.bytelegend;
 
 public class Cat {
-    public String name = "";
+    public String name;
 
     public static void main(String[] args) {
         Cat tom = createNewCat("Tom");
         tom.sayName();
     }
 
-    public Cat createNewCat(String name) {
-        this.name = name;
+    public static Cat createNewCat(String name) {
         return new Cat();
     }
 
     // Print my `name` to standard output
     public void sayName() {
-        System.out.println("My name is " + name + ", my name's length is " + name.length());
+        if (!name.isEmpty()) {
+            System.out.println("My name is " + name + ", my name's length is " + name.length());
+        } else {
+            System.out.println("My name is , my name's length is 0");
+        }
+        
     }
 }
