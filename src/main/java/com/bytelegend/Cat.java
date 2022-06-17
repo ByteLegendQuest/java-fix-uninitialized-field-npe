40e029b2eb98a3df2de4b7d43574107147ac8d02
karolk75 <karolk410410@gmail.com>
2022-06-17 19:32:54 +0000

    Update at 2022-06-17T19:32:54.554719Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -14,7 +14,7 @@ public class Cat {
 
     // Print my `name` to standard output
     public void sayName() {
-        if (!name.isEmpty()) {
+        if (!name.isNull()) {
             System.out.println("My name is " + name + ", my name's length is " + name.length());
         } else {
             System.out.println("My name is , my name's length is 0");
