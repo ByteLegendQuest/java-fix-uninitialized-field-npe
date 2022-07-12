522eb6693dfdaf1d425c8db52a43876012fff87e
chesterbachelor <kawalerbartosz@gmail.com>
2022-07-12 21:43:52 +0000

    Update at 2022-07-12T21:43:52.614148Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -2,6 +2,12 @@ package com.bytelegend;
 
 public class Cat {
     public String name;
+    public Cat(String name) {
+        this.name = name;
+    }
+    public Cat() {
+        this("Michael")
+    }
 
     public static void main(String[] args) {
         Cat tom = createNewCat("Tom");
