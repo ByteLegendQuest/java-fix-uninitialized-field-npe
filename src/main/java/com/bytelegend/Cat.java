f315af0cfbe728e452e717468f9ba245ac19c2e7
Newelle <wozi1988@proton.me>
2022-11-14 05:53:59 +0000

    Update at 2022-11-14T05:53:59.108188Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -9,13 +9,15 @@ public class Cat {
     }
 
     public static Cat createNewCat(String name) {
-        cat = new Cat()
+        cat = new Cat();
         cat.name = name;
         return cat;
     }
 
     // Print my `name` to standard output
     public void sayName() {
+        cat = new Cat();
+        name = cat.name;
         System.out.println("My name is " + name + ", my name's length is " + name.length());
     }
 }
