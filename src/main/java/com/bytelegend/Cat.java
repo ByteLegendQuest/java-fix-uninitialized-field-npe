4b236981ecfd192c1da1c7a2a440301f6025886f
19138060480 <703791321@qq.com>
2022-12-22 06:08:56 +0000

    Update at 2022-12-22T06:08:56.944147Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -2,6 +2,9 @@ package com.bytelegend;
 
 public class Cat {
     public String name;
+    public Cat(String name){
+        this.name=name;
+    }
 
     public static void main(String[] args) {
         Cat tom = createNewCat("Tom");
@@ -9,7 +12,7 @@ public class Cat {
     }
 
     public static Cat createNewCat(String name) {
-        return new Cat();
+        return new Cat(name);
     }
 
     // Print my `name` to standard output
