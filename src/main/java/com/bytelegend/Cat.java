161aad408602a46ac02a31c0e6a79818a294095a
wwwww1wwwww <1302780202@qq.com>
2022-12-25 13:35:58 +0000

    Update at 2022-12-25T13:35:58.692603Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -11,6 +11,9 @@ public class Cat {
     public static Cat createNewCat(String name) {
         return new Cat();
     }
+    public Cat(String name){
+        this.name = name;
+    }
 
     // Print my `name` to standard output
     public void sayName() {
